b9465f721d9dc7714f9e54d5d10c6bc5a17f8405
Joscha <34318751+josxha@users.noreply.github.com>
2026-02-17 19:51:56 +0100

    remove ServusTV, add tagesschau24

--- bin/updater.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-import 'm3u_service.dart';
-
-Future<void> main(List<String> args) async {
-  final responseString = await M3uService.getFilteredString();
-  final file = File("german-tv.m3u");
-  await file.writeAsString(responseString, mode: FileMode.writeOnly);
-  print("success! 😎");
-}
